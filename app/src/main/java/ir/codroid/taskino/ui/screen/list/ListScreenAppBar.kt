eb7f7f1b1855256bd3c1f7cba58db57d1c04ed60
Amirhossein <119420193+AmirMousavi-dev@users.noreply.github.com>
2023-07-01 18:04:55 +0330

    create SearchAction

--- app/src/main/java/ir/codroid/taskino/ui/screen/list/ListScreenAppBar.kt
+++ app/src/main/java/ir/codroid/taskino/ui/screen/list/ListScreenAppBar.kt
@@ -1,7 +1,10 @@
 package ir.codroid.taskino.ui.screen.list
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -10,20 +13,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import ir.codroid.taskino.R
 
 @Composable
-fun ListAppbar () {
-    DefaultListAppbar()
+fun ListAppbar(onSearchClicked: () -> Unit) {
+    DefaultListAppbar(onSearchClicked = onSearchClicked)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultListAppbar() {
-    SmallTopAppBar(
-        title = { Text(text = stringResource(id = R.string.tasks))}
+fun DefaultListAppbar(onSearchClicked: () -> Unit) {
+    TopAppBar(
+        title = { Text(text = stringResource(id = R.string.tasks)) },
+        actions = {
+            SearchAction(onSearchClicked = onSearchClicked)
+        }
     )
 }
 
 @Composable
+fun SearchAction(onSearchClicked: () -> Unit) {
+    IconButton(onClick = { onSearchClicked() }) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(id = R.string.search_task)
+        )
+    }
+}
+
+
+@Composable
 @Preview
-fun PreviewListAppbar () {
-    ListAppbar()
+fun PreviewListAppbar() {
+    ListAppbar({})
 }
