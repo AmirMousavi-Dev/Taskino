f00642eeb5e054aee84058d68a7267004d768585
Amirhossein <119420193+AmirMousavi-dev@users.noreply.github.com>
2023-07-02 20:06:02 +0330

    change SharedViewModel to ListScreenViewModel

--- app/src/main/java/ir/codroid/taskino/ui/viewmodel/ListScreenViewModel.kt
+++ app/src/main/java/ir/codroid/taskino/ui/viewmodel/ListScreenViewModel.kt
@@ -7,12 +7,11 @@ import ir.codroid.taskino.data.model.ToDoTask
 import ir.codroid.taskino.repository.TodoRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SharedViewModel @Inject constructor(private val repository: TodoRepository) : ViewModel() {
+class ListScreenViewModel @Inject constructor(private val repository: TodoRepository) : ViewModel() {
 
     private val _taskList = MutableStateFlow<List<ToDoTask>>(emptyList())
     val taskList = _taskList.asStateFlow()
