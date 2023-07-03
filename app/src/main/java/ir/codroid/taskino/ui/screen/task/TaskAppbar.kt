94cb54fbe73c05de5376a363360c4c045e2ef69c
Amirhossein <119420193+AmirMousavi-dev@users.noreply.github.com>
2023-07-03 21:30:53 +0330

    create Add New Task Appbar

--- app/src/main/java/ir/codroid/taskino/ui/screen/task/TaskAppbar.kt
+++ app/src/main/java/ir/codroid/taskino/ui/screen/task/TaskAppbar.kt
@@ -2,6 +2,7 @@ package ir.codroid.taskino.ui.screen.task
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -10,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import ir.codroid.taskino.R
 import ir.codroid.taskino.ui.theme.topAppbarColor
 import ir.codroid.taskino.ui.theme.topAppbarContentColor
 import ir.codroid.taskino.util.Action
@@ -17,18 +21,22 @@ import ir.codroid.taskino.util.Action
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskAppbar(
-    navigateToListScreen : (Action) -> Unit
+    navigateToListScreen: (Action) -> Unit
 ) {
     TopAppBar(
         title = {
-            Text(text = "Add new Task")
+            Text(text = stringResource(id = R.string.add_task))
         },
         colors = TopAppBarDefaults.smallTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.topAppbarColor,
             titleContentColor = MaterialTheme.colorScheme.topAppbarContentColor,
-            actionIconContentColor = MaterialTheme.colorScheme.topAppbarContentColor
+            actionIconContentColor = MaterialTheme.colorScheme.topAppbarContentColor,
+            navigationIconContentColor = MaterialTheme.colorScheme.topAppbarContentColor
         ),
         actions = {
+            AddAction(onAddClick = navigateToListScreen)
+        },
+        navigationIcon = {
             BackAction(onBackClick = navigateToListScreen)
         }
 
@@ -40,6 +48,27 @@ fun BackAction(
     onBackClick: (Action) -> Unit
 ) {
     IconButton(onClick = { onBackClick(Action.NO_ACTION) }) {
-        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+        Icon(
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription = stringResource(id = R.string.back_arrow)
+        )
     }
+}
+
+@Composable
+fun AddAction(
+    onAddClick: (Action) -> Unit
+) {
+    IconButton(onClick = { onAddClick(Action.ADD) }) {
+        Icon(
+            imageVector = Icons.Filled.Check,
+            contentDescription = stringResource(id = R.string.add_task)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PreviewTaskAppbar() {
+    TaskAppbar(navigateToListScreen = {})
 }
