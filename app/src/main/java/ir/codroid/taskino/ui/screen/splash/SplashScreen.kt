8dcedfc24a17efb744967eab95bc1ecde9c5fd75
Amirhossein <119420193+AmirMousavi-dev@users.noreply.github.com>
2023-07-14 20:51:34 +0330

    add animation to  splash screen

--- app/src/main/java/ir/codroid/taskino/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/ir/codroid/taskino/ui/screen/splash/SplashScreen.kt
@@ -1,5 +1,8 @@
 package ir.codroid.taskino.ui.screen.splash
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -7,14 +10,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -36,26 +44,52 @@ fun SplashScreen(
     navigateToTaskScreen: () -> Unit
 
 ) {
-    LaunchedEffect(key1 = true){
+    var startAnimation by remember { mutableStateOf(false) }
+    val offsetState by animateDpAsState(
+        targetValue = if (startAnimation) 0.dp else 100.dp,
+        animationSpec = tween(
+            1000
+        )
+    )
+    val alphaState by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            1000
+        )
+    )
+
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
         delay(SPLASH_SCREEN_DELAY)
         navigateToTaskScreen()
     }
     Column(
-        modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.splashBGColor),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.splashBGColor),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(id = R.drawable.taskino), contentDescription = stringResource(
+            painter = painterResource(id = R.drawable.taskino),
+            contentDescription = stringResource(
                 id = R.string.app_name
-            ) ,
+            ),
             modifier = Modifier
                 .weight(0.8f)
                 .fillMaxSize()
+                .offset(y = offsetState)
+                .alpha(alpha = alphaState)
                 .padding(SPLASH_LOGO_PADDING),
         )
-        Box(modifier = Modifier.weight(0.2f), contentAlignment = Alignment.BottomCenter) {
+        Box(
+            modifier = Modifier
+                .weight(0.2f)
+                .offset(y = offsetState)
+                .alpha(alphaState), contentAlignment = Alignment.BottomCenter
+        ) {
 
-        LoadingCircle(isSystemDark = false)
-    }}
+            LoadingCircle(isSystemDark = false)
+        }
+    }
 }
